5d64240ab5d2c78836aa4e9da162b342ae63fbda
Steve Kondik <shade@chemlab.org>
2014-01-26 01:15:42 -0800

    hh: Enable qrngd
     * Improves boot time
    
    Change-Id: I524d4fb248b0103543703eae923267b0c410b3ef
    
    Conflicts:
    	init.hammerhead.rc

--- device.mk
+++ device.mk
@@ -201,6 +201,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bdAddrLoader
 
+# msm_rng entropy feeder
+PRODUCT_PACKAGES += \
+    qrngd \
+    qrngp
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
 
